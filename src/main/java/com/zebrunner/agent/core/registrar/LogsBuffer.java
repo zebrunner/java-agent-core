04835d77ebaabbf02707ad88daabd6d07d947d70
sbrenko <sbrenko@solvd.com>
2020-09-17 12:51:08 +0300

    Remove warn logging from LogBuffer in order to avoid stackoverflow for log4j and log4j2

--- src/main/java/com/zebrunner/agent/core/registrar/LogsBuffer.java
+++ src/main/java/com/zebrunner/agent/core/registrar/LogsBuffer.java
@@ -2,20 +2,15 @@ package com.zebrunner.agent.core.registrar;
 
 import com.zebrunner.agent.core.appender.Log;
 import com.zebrunner.agent.core.config.ConfigurationHolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.function.Function;
 
 public interface LogsBuffer<E> {
 
-    Logger LOGGER = LoggerFactory.getLogger(TestRunRegistrar.class);
-
     static <E> LogsBuffer<E> create(Function<E, Log> converter) {
         if (ConfigurationHolder.isReportingEnabled()) {
             return new FlushingLogsBuffer<>(converter);
         } else {
-            LOGGER.warn("Reporting disabled: using no op logs buffer");
             return new NoOpLogsBuffer<>();
         }
     }
