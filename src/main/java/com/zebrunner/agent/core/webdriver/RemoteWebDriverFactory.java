357ad8af73e3fa55ca378057592cbbf51de2b45d
sbrenko <sbrenko@solvd.com>
2021-05-07 12:43:08 +0300

    Add factory for remote web drivers

---
+++ src/main/java/com/zebrunner/agent/core/webdriver/RemoteWebDriverFactory.java
@@ -0,0 +1,59 @@
+package com.zebrunner.agent.core.webdriver;
+
+import com.zebrunner.agent.core.registrar.domain.ObjectMapperImpl;
+import kong.unirest.GenericType;
+import kong.unirest.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.Map;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RemoteWebDriverFactory {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapperImpl();
+    private static final GenericType<Map<String, String>> CAPABILITIES_TYPE = new GenericType<Map<String, String>>() {
+    };
+
+    @SneakyThrows
+    public static URL getSeleniumHubUrl() {
+        String hubUrl = System.getenv("ZEBRUNNER_HUB_URL");
+        if (hubUrl == null) {
+            throw new RuntimeException("You cannot");
+        }
+        return new URL(hubUrl);
+    }
+
+    public static Capabilities getCapabilities() {
+        String capabilities = System.getenv("ZEBRUNNER_CAPABILITIES");
+
+        Capabilities desiredCapabilities = new DesiredCapabilities();
+        if (capabilities != null) {
+            Map<String, String> capabilitiesMap = OBJECT_MAPPER.readValue(capabilities, CAPABILITIES_TYPE);
+            desiredCapabilities = new DesiredCapabilities(capabilitiesMap);
+        }
+
+        return desiredCapabilities;
+    }
+
+    public static RemoteWebDriver getDriver() {
+        return new RemoteWebDriver(getSeleniumHubUrl(), getCapabilities());
+    }
+
+    public static RemoteWebDriver getDriver(Capabilities capabilities) {
+        capabilities = capabilities.merge(getCapabilities());
+        return new RemoteWebDriver(getSeleniumHubUrl(), capabilities);
+    }
+
+    public static RemoteWebDriver getDriver(Map<String, ?> capabilitiesMap) {
+        Capabilities capabilities = new DesiredCapabilities(capabilitiesMap);
+        capabilities = capabilities.merge(getCapabilities());
+        return new RemoteWebDriver(getSeleniumHubUrl(), capabilities);
+    }
+
+}
